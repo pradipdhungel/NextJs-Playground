561e4436dcfba7af388964b0a964b6b544082325
pradipdhungel <pradipdhungel12@gmail.com>
2023-12-30 14:52:51 -0600

    just change 111

--- my-app-next-js/src/app/page.tsx
+++ my-app-next-js/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           Get started by editing
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <p>Pradip</p>
+        <p>Pradipn 111</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
